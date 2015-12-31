333fb72f3e1176a2abc64d07ec9acd79f329148b
giganoide <giganoide@hotmail.com>
2015-12-31 11:20:36 +0100

    Add acceptance test

--- PrisonerHatRiddle/Class1.cs
+++ PrisonerHatRiddle/Class1.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace PrisonerHatRiddle
 {
-    public class Class1
+    public class Prisoners
     {
+        public string GetAllResponses(string hats) { return ""; }
     }
 }
